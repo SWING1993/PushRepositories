f800a49bc1ff0d9f0ebfd9b56dbdfe40e9681488
SWING1993 <10446627@qq.com>
2019-01-25 11:51:15 +0800

    orz -.-

--- push.sh
+++ push.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "Hello World!"
 
-
-
-
 #设置变量key，存储密码的所有可能性（密码库），如果还需要其他字符请自行添加其他密码字符
 #使用$#统计密码库的长度
 key="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -12,15 +9,14 @@ num=${#key}
 pass=''
 #循环8次，生成8为随机密码
 #每次都是随机数对密码库的长度取余，确保提取的密码字符不超过密码库的长度
-#每次循环提取一位随机密码，并将该随机密码追加到pass变量的最后
 for i in {1..8}
 do
     index=$[RANDOM%num]
     pass=$pass${key:$index:1}
 done
-echo $pass
- 
+
 touch $pass
+
 git add .
 git commit -m 'orz -.-'
 git push
