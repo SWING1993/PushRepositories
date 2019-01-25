1cf6450c6dc13dcc0c6c4a1815ca850cf72125d3
SWING1993 <10446627@qq.com>
2019-01-25 11:48:52 +0800

    orz -.-

--- push.sh
+++ push.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 echo "Hello World!"
 
-function rand(){
-    min=1
-    max=99999999
-    num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
-    echo $(($num%$max+$min))
-}
+
+
+
+#设置变量key，存储密码的所有可能性（密码库），如果还需要其他字符请自行添加其他密码字符
+#使用$#统计密码库的长度
+key="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num=${#key}
+#设置初始密码为空
+pass=''
+#循环8次，生成8为随机密码
+#每次都是随机数对密码库的长度取余，确保提取的密码字符不超过密码库的长度
+#每次循环提取一位随机密码，并将该随机密码追加到pass变量的最后
+for i in {1..8}
+do
+    index=$[RANDOM%num]
+    pass=$pass${key:$index:1}
+done
+echo $pass
  
-rnd=$(rand)
-echo $rnd
-touch $rnd
+touch $pass
 git add .
 git commit -m 'orz -.-'
 git push
