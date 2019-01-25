2f5e352e48c45bc9d5481b2461976fdfa91b4bdd
SWING1993 <10446627@qq.com>
2019-01-25 11:43:18 +0800

    orz -.-

--- push.sh
+++ push.sh
@@ -2,13 +2,13 @@
 echo "Hello World!"
 
 function rand(){
-    min=$1
-    max=$(($2-$min+1))
+    min=1
+    max=99999999
     num=$(cat /dev/urandom | head -n 10 | cksum | awk -F ' ' '{print $1}')
     echo $(($num%$max+$min))
 }
  
-rnd=$(rand 100 500)
+rnd=$(rand)
 echo $rnd
 touch $rnd
 git add .
