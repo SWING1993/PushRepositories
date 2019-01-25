1710941c2af9a4cda9856f40d3d713c9e5f1aea4
SWING1993 <10446627@qq.com>
2019-01-25 11:59:04 +0800

    orz -.-

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Hello World!"
 
+#rm -rf 
+
 #设置变量key，存储密码的所有可能性（密码库），如果还需要其他字符请自行添加其他密码字符
 #使用$#统计密码库的长度
 key="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -15,7 +17,7 @@ do
     pass=$pass${key:$index:1}
 done
 
-touch $pass
+touch bulid/$pass
 
 git add .
 git commit -m 'orz -.-'
