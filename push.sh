d4efb3b35e5377a7ef79aabc65b9a50d4e0364ed
SWING1993 <10446627@qq.com>
2019-01-25 14:46:17 +0800

    orz -.-

--- push.sh
+++ push.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 echo "Hello World!"
 
-rm -rf /root/GitRepositories/PushRepositories/build/*
-
-#设置变量key，存储密码的所有可能性（密码库），如果还需要其他字符请自行添加其他密码字符
-#使用$#统计密码库的长度
 key="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 num=${#key}
-#设置初始密码为空
 pass=''
-#循环8次，生成8为随机密码
-#每次都是随机数对密码库的长度取余，确保提取的密码字符不超过密码库的长度
 for i in {1..8}
 do
     index=$[RANDOM%num]
     pass=$pass${key:$index:1}
 done
 
+rm -rf /root/GitRepositories/PushRepositories/build/*
 cd /root/GitRepositories/PushRepositories/build/
 touch $pass
 
