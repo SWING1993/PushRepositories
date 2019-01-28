b9f5c2bab0532e0fa3513dcc2f12ef74f199f555
SWING1993 <10446627@qq.com>
2019-01-28 15:06:51 +0800

    orz -.-

--- push.sh
+++ push.sh
@@ -21,8 +21,8 @@ git push origin master
 git push gitlab master
 
 
-
 # orange
+echo "orange"
 cd /root/GitRepositories/orange/
 git pull origin
 git push github --all
@@ -30,6 +30,7 @@ git push github --tags
 
 
 # MPM_Packstone
+echo "Packstone"
 cd /root/GitRepositories/MPM_Packstone/
 git pull origin
 git push github --all
@@ -37,6 +38,7 @@ git push github --tags
 
 
 # TomatoPulp
+echo "TomatoPulp"
 cd /root/GitRepositories/TomatoPulp/
 git pull origin
 git push github --all
@@ -44,6 +46,7 @@ git push github --tags
 
 
 # index
+echo "index"
 cd /root/GitRepositories/index/
 git pull origin
 git push github --all
@@ -51,7 +54,11 @@ git push github --tags
 
 
 # Tomato
+echo "Tomato"
 cd /root/GitRepositories/Tomato/
 git pull origin
 git push github --all
 git push github --tags
+
+
+
