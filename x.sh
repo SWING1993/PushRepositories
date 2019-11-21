748274b5e879cf9655a3c8eb188efbb4b3d4c783
Gitea <gitea@fake.local>
2019-11-21 15:19:31 +0800

    update

--- x.sh
+++ x.sh
@@ -3,7 +3,7 @@ echo "Hello World !"
 rm -rf /root/GitRepositories/orz/build/*
 cd /root/GitRepositories/orz/build/
 touch $RANDOM
-cd /root/GitRepositories/orz
-git add -A
+cd /root/GitRepositories/orz/
+git add --all .
 git commit -m "update"
 git push
