ccd61c4cf39b629ad70c5e7f1c944b145ca41ad3
Gitea <gitea@fake.local>
2020-04-21 09:30:43 +0800

    update

--- x.sh
+++ x.sh
@@ -2,10 +2,8 @@
 echo "Hello World !"
 rm -rf /root/GitRepositories/orz/build/*
 cd /root/GitRepositories/orz/build/
-touch config
-
+#touch config
 echo "$RANDOM">>config
-
 cd /root/GitRepositories/orz/
 git add --all .
 git commit -m "update"
